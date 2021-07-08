ac997806e0d3d9e9b71b4d4d530b48d1c731ec1b
PhenoM4n4n <phenom4n4n@gmail.com>
2021-07-07 19:51:28 -0700

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from distutils.core import setup
-import re
+from setuptools import setup
 
-VERSION_RE = re.compile(r"__version__\s*=\s*\"(\d+\.\d+\.\d+)\"")
-
-with open("./TagScriptEngine/__init__.py", "r") as init:
-    match = VERSION_RE.search(init.read())
-if not match:
-    raise RuntimeError("failed to find version info")
-version = match.group(1)
-
-setup(
-    name="TagScriptEngine",
-    url="https://github.com/phenom4n4n/TagScript",
-    author="PySnow",
-    author_email="vasti009@gmail.com",
-    version=version,
-    packages=[
-        "TagScriptEngine",
-        "TagScriptEngine.adapter",
-        "TagScriptEngine.block",
-        "TagScriptEngine.interface",
-    ],
-)
+setup()
